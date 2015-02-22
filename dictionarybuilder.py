60d1347736d11636907ca3f5d10fe86ef9953de6
ksadwin <kelly.sadwin.ic@gmail.com>
2015-02-22 03:43:27 -0500

    dictionaries: built
    did anybody order a side of pickles

--- dictionarybuilder.py
+++ dictionarybuilder.py
@@ -1,33 +1,44 @@
 import pickle
 import os
 
-def addToDict(picdic):
-    if os.path.exists(picdic):
-        dic = pickle.load(open(picdic, "rb"))
-        print("Dictionary loaded.")
-    else:
-        dic = {}
-        print("New dictionary created.")
-    adding = "n"
-    while adding == "n":
-        key = input("Enter new key: ")
-        if key in dic.keys():
-            print("Key already exists.")
-        else:
-            value = input("Enter new value: ")
-            dic[key] = value
-        adding = input("Done? (y/n)")
-    pickle.dump(dic, open(picdic, "wb"))
-        
+def addToDict(dic, filename):
+    f = open(filename, "r")
+    for line in f.readlines():
+        line = line.split(":")
+        if len(line) == 2:
+            dic[line[0]] = line[1]
+    f.close()
     
     
 
 def main():
-    print("Male to Female dictionary: mtf.p")
-    print("Female to Male dictionary: ftm.p")
-    print("Gender Neutral dictionary: gn.p")
-    picdic = input("Enter desired dictionary to edit: ")
-    addToDict(picdic)
+    #a dictionary for miscellaneous gendered terms (e.g. husband, fireman, etc.)
+    miscTerms = {}
+    addToDict(miscTerms, "MGender.txt")
+    addToDict(miscTerms, "Fgender.txt")
+    pickle.dump(miscTerms, open("misc.p", "wb"))
+    
+    #order: subjective, objective, possessive, possessive noun. PRP($) is for Stanford grammar
+    she = [("PRP", "she"), ("PRP", "her"), ("PRP$", "her"), ("PRP", "hers")]
+    he = [("PRP", "he"), ("PRP", "him"), ("PRP$", "his"), ("PRP", "his")]
+    they = [("PRP", "they"), ("PRP", "them"), ("PRP$", "their"), ("PRP", "their")]
+
+    #change any pronoun to they
+    toThey = {}
+    #change she pronouns to he
+    toHe = {}
+    #change he pronouns to she
+    toShe = {}
+    for i in range(4):
+        toThey[she[i]] = they[i]
+        toThey[he[i]] = they[i]
+        toShe[he[i]] = she[i]
+        toHe[she[i]] = he[i]
+    pickle.dump(toThey, open("toThey.p", "wb"))
+    pickle.dump(toShe, open("toShe.p", "wb"))
+    pickle.dump(toHe, open("toHe.p", "wb"))
+    
+    
     
 main()
         
